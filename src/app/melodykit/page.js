a9129fa60de67c471457d5c23e84642af8edecb3
Rivridis <sanjaypugal.personal@gmail.com>
2026-01-17 19:07:32 +0530

    melodykit update

--- src/app/melodykit/page.js
+++ src/app/melodykit/page.js
@@ -17,14 +17,19 @@ export default function Home() {
                       <Navbar />
                       </div>
                       <main className="flex flex-col items-center justify-center flex-1">
-                          <section id ="top" className="text-center max-w-3xl  min-h-screen items-center justify-center flex flex-col"> 
+                          <section id ="top" className="text-center max-w-3xl pt-40 items-center justify-center flex flex-col"> 
                             <h1 className="text-5xl md:text-7xl mb-4 text-[#ede9e5]">RIVRIDIS MELODYKIT</h1>
                             <p className="p-5 text-xl md:text-2xl text-[#ede9e5] mb-6">
                             MelodyKit is an easy to use DAW, which can be used for both hobby and professional music production. Try out the beta now!
                             </p>
-                            <Link href="https://discord.gg/eVKE3A3fH9" className="inline-block bg-[#5865F2] text-white px-6 py-3 rounded-lg font-semibold shadow hover:bg-[#4752C4] transition">
+                            <div className="mb-6 flex flex-col md:flex-row gap-4">
+                            <Link href="https://discord.gg/eVKE3A3fH9" className="inline-block bg-[#e2d5a6] text-#000000 px-6 py-3 rounded-lg font-semibold shadow hover:bg-[#4752C4] transition">
                             Join Discord for Beta
                             </Link>
+                            <Link href="https://www.patreon.com/c/Rivridis" className="inline-block bg-[#e2d5a6] text-#000000 px-6 py-3 rounded-lg font-semibold shadow hover:bg-[#d85743] transition">
+                            Download from Patreon
+                            </Link>
+                            </div>
                           </section>
                          
                 <div className="flex justify-center p-6">
